6492039179984a87fc3380095252357206b05711
cypherm0nk <88748637+cypherm0nk@users.noreply.github.com>
2025-04-26 21:02:54 -0300

    get weather from city

--- src/app/modules/weather/page/weather-home/weather-home.component.ts
+++ src/app/modules/weather/page/weather-home/weather-home.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
+import { WeatherDatas } from '../../../../models/interfaces/WeatherDatas';
+import { WeatherService } from './../../services/weather.service';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-weather-home',
   standalone: false,
   templateUrl: './weather-home.component.html',
 })
-export class WeatherHomeComponent {
+export class WeatherHomeComponent implements OnInit, OnDestroy {
+  private readonly destroy$: Subject<void> = new Subject();
+  initialCityName: string = 'London';
+  weatherDatas!: WeatherDatas;
+  searchIcon=faMagnifyingGlass;
 
+  constructor(private readonly weatherService: WeatherService) {}
+  ngOnInit(): void {
+    this.getWeatherDatas(this.initialCityName);
+  }
+  getWeatherDatas(cityName: string): void {
+    this.weatherService
+      .getWeatherDatas(cityName)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (response) => {
+          response && (this.weatherDatas = response);
+          console.log(this.weatherDatas);
+        },
+        error: (error) => console.log(error),
+      });
+  }
+  onSubmit(): void {
+    this.getWeatherDatas(this.initialCityName);
+    this.initialCityName = '';
+  }
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
